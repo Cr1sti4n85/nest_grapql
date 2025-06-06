40ca2739d3b06a5cb5570b6613c29097cd795151
Cristian Perez <cperezlecaros@gmail.com>
2025-06-06 16:26:45 -0400

    Update todo service

--- 1. foundation/src/todo/todo.service.ts
+++ 1. foundation/src/todo/todo.service.ts
@@ -1,19 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodoService {
-  create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+  private todos: Todo[] = [
+    {
+      id: 1,
+      description: 'Piedra del Alma',
+      done: false,
+    },
+    {
+      id: 2,
+      description: 'Piedra del Tiempo',
+      done: false,
+    },
+    {
+      id: 3,
+      description: 'Piedra del Espacio',
+      done: false,
+    },
+  ];
+
+  create(createTodoDto: CreateTodoDto): Todo {
+    const todo = new Todo();
+    todo.id = Math.max(...this.todos.map((todo) => todo.id), 0) + 1;
+    todo.description = createTodoDto.description;
+    this.todos.push(todo);
+    return todo;
   }
 
-  findAll() {
-    return `This action returns all todo`;
+  findAll(): Todo[] {
+    return this.todos;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} todo`;
+  findOne(id: number): Todo | string {
+    const todo = this.todos.find((todo) => todo.id === id);
+    return todo || 'No existe';
   }
 
   update(id: number, updateTodoDto: UpdateTodoDto) {
