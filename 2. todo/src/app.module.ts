b5ea8daeff1763feee7f951c7ad30b5763af9952
Cristian Perez <cperezlecaros@gmail.com>
2025-06-11 19:29:30 -0400

    Update resolver and app module

--- 2. todo/src/app.module.ts
+++ 2. todo/src/app.module.ts
@@ -3,13 +3,17 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      graphiql: true,
+      // graphiql: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      // plugins: [ApolloServer],
     }),
     HelloWorldModule,
   ],
--- 2. todo/src/hello-world/hello-world.resolver.ts
+++ 2. todo/src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,23 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
   //primer query, en elquery le digo lo que devuelve
-  @Query(() => String)
+  @Query(() => String, {
+    description: 'Retorna string de hola mundo',
+    name: 'salutation',
+  })
   helloWorld(): string {
     return 'Hola mundo';
+  }
+
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int)
+  getRandomFromZero(): number {
+    return Math.floor(Math.random() * 10);
   }
 }
