72d098fe080cb8e5fdb5584fae30ee1ef72c48b1
Cristian Perez <cperezlecaros@gmail.com>
2025-07-27 22:34:04 -0400

    Add search filter

--- 3. anylist/src/items/items.service.ts
+++ 3. anylist/src/items/items.service.ts
@@ -5,10 +5,12 @@ import {
 } from '@nestjs/common';
 import { CreateItemInput } from './dto/inputs/create-item.input';
 import { UpdateItemInput } from './dto/inputs/update-item.input';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { Item } from './entities/item.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { PaginationArgs } from '../common/dto/args/pagination.args';
+import { SearchArgs } from '../common/dto/args/search.args';
 
 @Injectable()
 export class ItemsService {
@@ -26,15 +28,32 @@ export class ItemsService {
     return newItem;
   }
 
-  async findAll(user: User): Promise<Item[]> {
-    const items = await this.itemsRepository.find({
-      where: {
-        user: {
-          id: user.id,
-        },
-      },
-    });
-    return items;
+  async findAll(
+    user: User,
+    paginationArgs: PaginationArgs,
+    searchArgs: SearchArgs,
+  ): Promise<Item[]> {
+    const { limit, offset } = paginationArgs;
+    const { search } = searchArgs;
+
+    // const items = await this.itemsRepository.find({
+    //   take: limit,
+    //   skip: offset,
+    //   where: {
+    //     user: {
+    //       id: user.id,
+    //     },
+    //     name: Like('%' + (search || '') + '%'),
+    //   },
+    // });
+    // return items;
+    const queryBuilder = this.itemsRepository
+      .createQueryBuilder()
+      .where('userId = :userId', { userId: user.id })
+      .take(limit)
+      .skip(offset);
+
+    return queryBuilder.getMany();
   }
 
   async findOne(id: string, user: User): Promise<Item> {
